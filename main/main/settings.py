609d8a515c7591f92544367845ab03ddf9ac40b7
Meggie <megych91@gmail.com>
2018-08-02 14:14:12 -0400

    Diamond is amazing

--- main/main/settings.py
+++ main/main/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'main.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'todo',
+        'NAME': 'ecommerce',
         'USER': 'root',
         'PASSWORD': 'root',
         'HOST': 'localhost',
