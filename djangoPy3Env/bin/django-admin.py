5f849e7c57f16e97bada7d6045f5873322b5f8cf
Jose <sixgg4@gmail.com>
2018-08-02 23:25:34 -0400

    Fixed bcrypt issue
    Removed  env, added requirements text.

--- djangoPy3Env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/paulinosantos/Desktop/django_group_project/Ecommerce_Django/djangoPy3Env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
