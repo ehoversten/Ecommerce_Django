a55f4480fdcbebba20831f68807592a364fe5b37
paulino <psantosiii@yahoo.com>
2018-08-03 17:20:15 -0400

    order model

---
+++ main/apps/order/views.py
@@ -0,0 +1,9 @@
+
+from django.shortcuts import render
+from django.http import Http404, JsonResponse
+from django.views.generic import View, ListView, DetailView
+from django.shortcuts import render
+
+# Create your views here.
+
+    
