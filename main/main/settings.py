e7f494db3138c84c4acad4f942d279b98cdb4ea1
ehoversten <ehoversten@gmail.com>
2018-08-30 23:49:46 -0400

    basic form styling

--- main/main/settings.py
+++ main/main/settings.py
@@ -108,7 +108,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ecommerce',
         'USER': 'root',
-        'PASSWORD':'',
+        'PASSWORD':'root',
         'HOST': 'localhost',
         'PORT': '3306',
     }
