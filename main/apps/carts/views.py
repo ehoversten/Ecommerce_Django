3d8a410dba7adf62d23f2242ffc0efea93ea7eff
ehoversten <ehoversten@gmail.com>
2018-11-15 18:46:06 -0500

    added session to Cart view

--- main/apps/carts/views.py
+++ main/apps/carts/views.py
@@ -1,15 +1,34 @@
 from django.shortcuts import render, redirect
 
+from .models import Cart
+
+#  CREATE CART METHOD
+def cart_create(user=None):
+    cart_obj = Cart.objects.create(user=None)
+    print("New Cart Created")
+    return cart_obj
+
 # Create your views here.
 def cart_home(request):
+    #### reset the session variable for testing ####
+    # del request.session['cart_id']
+
+    print('*'*50)
     cart_id = request.session.get("cart_id", None)
-    if cart_id is None:
-        print("create new cart")
-        request.session['cart_id'] = 5
-    else:
+    # if cart_id is None:
+    #     cart_obj = cart_create()
+    #     request.session['cart_id'] = cart_obj.id
+    #     # print('*'*50)
+    # else:
+    qs = Cart.objects.filter(id=cart_id)
+    if qs.exists and qs.count == 1:
         print('Cart ID exists: ', cart_id)
-    # if user is logged in assign to session
-    # request.session['user'] = request.session.username
+        print('*'*50)
+        cart_obj = qs.first()
+    else:
+        cart_obj = cart_create()
+        print("New Cart ID: ", cart_id)
+        request.session['cart_id'] = cart_obj.id
 
     context = {
 
