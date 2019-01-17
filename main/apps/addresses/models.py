bb22763ff83105b8f67b2c4660f2f49486e9d0aa
ehoversten <ehoversten@gmail.com>
2019-01-17 15:11:27 -0500

    added address model

--- main/apps/addresses/models.py
+++ main/apps/addresses/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+from apps.billing.models import BillingProfile
+
+ADDRESS_TYPES = (
+    ('billing', 'Billing'),
+    ('shipping', 'Shipping'),
+)
+
 # Create your models here.
+class Address(Model.model):
+    billing_profile = models.ForeignKey(BillingProfile)
+    address_type    = models.CharField(max_length=120, choices=ADDRESS_TYPES)
+    address_1       = models.CharField(max_length=120)
+    address_2       = models.CharField(max_length=120, null=True, blank=True)
+    city            = models.CharField(max_length=120)
+    state           = models.CharField(max_length=120)
+    country         = models.CharField(max_length=120, default="United States")
+    postal_code     = models.CharField(max_length=120)
+
+    # def __repr__(self):
+    #     return str(self.billing_profile)
+
+    def __str__(self):
+        return str(self.billing_profile)
