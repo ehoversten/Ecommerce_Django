04d604ae7eca16321394f070aeea19bf3b45058c
ehoversten <ehoversten@gmail.com>
2019-01-22 23:25:34 -0500

    added shipping and billing addresses association in Orders model

--- main/apps/orders/models.py
+++ main/apps/orders/models.py
@@ -2,6 +2,7 @@ import math
 from django.db import models
 from django.db.models.signals import pre_save, post_save
 
+from apps.addresses.models import Address
 from apps.carts.models import Cart
 from apps.billing.models import BillingProfile
 from main.utils import unique_order_id_generator
@@ -38,16 +39,16 @@ class OrderManager(models.Manager):
         return obj, created
 
 class Order(models.Model):
+    billing_profile  = models.ForeignKey(BillingProfile, null=True, blank=True)
+    shipping_address = models.ForeignKey(Address, related_name="shipping_address", null=True, blank=True)
+    billing_address  = models.ForeignKey(Address, related_name="billing_address", null=True, blank=True)
+    cart             = models.ForeignKey(Cart)
     # pk / id -> unique, random
-    order_id        = models.CharField(max_length=120, blank=True)
-    billing_profile = models.ForeignKey(BillingProfile, null=True, blank=True)
-    # shipping_address
-    # billing_address
-    cart           = models.ForeignKey(Cart)
-    status         = models.CharField(max_length=120, default='created', choices=ORDER_STATUS_CHOICES)
-    shipping_total = models.DecimalField(default=5.99, max_digits=7, decimal_places=2)
-    total          = models.DecimalField(default=0.00, max_digits=7, decimal_places=2)
-    active         = models.BooleanField(default=True)
+    order_id         = models.CharField(max_length=120, blank=True)
+    status           = models.CharField(max_length=120, default='created', choices=ORDER_STATUS_CHOICES)
+    shipping_total   = models.DecimalField(default=5.99, max_digits=7, decimal_places=2)
+    total            = models.DecimalField(default=0.00, max_digits=7, decimal_places=2)
+    active           = models.BooleanField(default=True)
 
     def __str__(self):
         return self.order_id
